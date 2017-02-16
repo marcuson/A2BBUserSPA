253fa05541840bb355d33cc63e144835201b016f
Leonardo Marcucci <l.marcucci@sintraconsulting.eu>
2017-02-16 16:18:23 +0100

    Create QR code component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { A2BBAuthService } from './services/a2bb-auth.service';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { QRCodeComponent } from './components/qrcode/qrcode.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DashboardComponent
+    DashboardComponent,
+    QRCodeComponent
   ],
   imports: [
     BrowserModule,
